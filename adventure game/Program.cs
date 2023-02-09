2816542f2e00d1d3ddbe329f539c25cfcaf26a9b
mayam <mayam@LAPTOP-ASFPID31>
2023-02-09 18:56:57 +0000

    Add project files.

---
+++ adventure game/Program.cs
@@ -0,0 +1,12 @@
+﻿string playerName = Console.ReadLine();
+
+static void intro(string name)
+{
+    Console.WriteLine("The fire is blazing, and a young, curious person is sitting slouched on the sofa. Bored out of their mind, they look around for something to do: ");
+    Console.WriteLine("A deep green guitar sits in the corner. ");
+    Console.WriteLine("A notebook lies in front. ");
+    Console.WriteLine("A large window is behind.");
+    Console.WriteLine("The person is you. Who are you? Enter your name.");
+    name = Console.ReadLine();
+}
+
